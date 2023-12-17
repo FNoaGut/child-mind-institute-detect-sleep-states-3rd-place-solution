6c8a4d5aa6bea636db602927c27dfd91b8324798
fnoa <paco.noa.gut@gmail.com>
2023-12-17 15:30:04 +0100

    second commit

--- preprocessing/runs_prep.py
+++ preprocessing/runs_prep.py
@@ -4,14 +4,14 @@ import pandas as pd
 
 codigo = 'preprocess'
 
-# # CREATE TEST
-# mode = 'test'
-# pure_calculations = 1
-# name = 'basic'
-# downsample = 6
-# clean = 0
-# invert_series = 0
-# os.system(f'python {codigo}.py --mode {mode} --name {name} --calculations {pure_calculations} --downsample {downsample} --clean {clean} --invert_series {invert_series}')
+# CREATE TEST
+mode = 'test'
+pure_calculations = 1
+name = 'basic'
+downsample = 6
+clean = 0
+invert_series = 0
+os.system(f'python {codigo}.py --mode {mode} --name {name} --calculations {pure_calculations} --downsample {downsample} --clean {clean} --invert_series {invert_series}')
 
 # CREATE TRAIN
 mode = 'train'
